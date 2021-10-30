6fac630004c259b7fd2450615fcecc799081d773
Osamu Miyazawa <miyazawa.o.aa@m.titech.ac.jp>
2021-10-30 22:37:42 +0900

    M5StickCの例を修正。

--- examples/M5StickCWebRadio/main.cpp
+++ examples/M5StickCWebRadio/main.cpp
@@ -7,30 +7,37 @@
 #define SSID "********"
 #define PASSWD "********"
 
-bool isMute = false;
 M3U8Player *player;
+
+void print(const char *text) {
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.printf("%s", text);
+}
 
 void setup() {
   M5.begin();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.setTextSize(2);
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWD);
-  M5.Lcd.printf("WiFi Connecting...\n");
+  print("WiFi Connecting...");
   log_i("WiFi Connecting...");
   while (!WiFi.isConnected()){ delay(10); }
-  M5.Lcd.printf("WiFi Connected\n");
+  print("WiFi Connected");
   log_i("WiFi Connected");
 
   String stationUrl = "http://xxxxx/xxxxx/xxxxx.m3u8"; // Write URL of m3u8 here
   player = new M3U8Player(stationUrl, 100.0);
   delay(3000);
+  print("setup completed");
   log_i("setup completed");
-  M5.Lcd.printf("setup completed\n");
-  M5.Lcd.setTextSize(2);
 }
 
 void loop() {
   M5.update();
   if (M5.BtnA.isPressed()){
+    print("now playing");
     player->playAAC();
   }
   delay(10);
