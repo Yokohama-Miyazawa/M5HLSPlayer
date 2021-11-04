282236c63b233b5512add85d5248644bdd6a42dd
Osamu Miyazawa <miyazawa.o.aa@m.titech.ac.jp>
2021-11-05 02:42:39 +0900

    M5Stack用のサンプルを修正。

--- examples/M5StackWebRadio/main.cpp
+++ examples/M5StackWebRadio/main.cpp
@@ -7,6 +7,15 @@
 #define PASSWD "********"
 
 M3U8Player *player;
+float currentVolume;
+float maxVolume = 100.0;
+float volumeStep = 5.0;
+
+void showVolume(const float &volume) {
+  M5.Lcd.setCursor(10, 20);
+  M5.Lcd.printf("Volume: %03.0f", volume);
+  return;
+}
 
 void setup() {
   M5.begin();
@@ -19,17 +28,32 @@ void setup() {
   log_i("WiFi Connected");
 
   String stationUrl = "http://xxxxx/xxxxx/xxxxx.m3u8";  // Write URL of m3u8 here
-  player = new M3U8Player(stationUrl);
+  float initialVolume = 5.0;
+  player = new M3U8Player(stationUrl, initialVolume);
   delay(3000);
   log_i("setup completed");
   M5.Lcd.printf("setup completed\n");
-  M5.Lcd.setTextSize(2);
+  player->start();
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.clear(BLACK);
+  showVolume(initialVolume);
 }
 
 void loop() {
   M5.update();
-  if (M5.BtnB.isPressed()){
-    player->playAAC();
+  if (M5.BtnA.isPressed()){
+    currentVolume = player->getVolume();
+    if(currentVolume >= volumeStep){
+      player->setVolume(currentVolume - volumeStep);
+      showVolume(currentVolume - volumeStep);
+    }
+  }
+  if (M5.BtnC.isPressed()){
+    currentVolume = player->getVolume();
+    if(currentVolume <= maxVolume - volumeStep){
+      player->setVolume(currentVolume + volumeStep);
+      showVolume(currentVolume + volumeStep);
+    }
   }
   delay(10);
 }
